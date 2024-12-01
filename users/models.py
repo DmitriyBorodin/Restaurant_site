3453a002df8dc6a5a9dfb1e1ea1b9785d7ea61a7
Dmitriy Borodin <dmitriyborodinpm@gmail.com>
2024-12-01 10:25:41 +0300

    added .env.sample, created users model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+NULLABLE = {"blank": True, "null": True}
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name="Почта")
+    phone = PhoneNumberField(unique=True, verbose_name="Телефон")
+    first_name = models.CharField(max_length=50, verbose_name="Имя")
+    last_name = models.CharField(max_length=50, verbose_name="Фамилия", **NULLABLE)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+    def __str__(self):
+        return self.email
